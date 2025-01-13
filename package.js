e3f11abe11fa9868fbc20e100ede8b32d5f848fe
jankapunkt <jkuester@uni-bremen.de>
2025-01-13 22:37:45 +0100

    deps: upgrade for 3.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* eslint-env meteor */
 Package.describe({
   name: 'jkuester:autoform-password2',
-  version: '2.1.0',
+  version: '3.0.0-rc.0',
   // Brief, one-line summary of the package.
   summary: 'Autoform password input using Bootstrap 4 and with extra functionality.',
   // URL to the Git repository containing the source code for this package.
@@ -12,18 +12,19 @@ Package.describe({
 })
 
 Package.onUse(function (api) {
-  api.versionsFrom(['1.6', '2.3'])
+  api.versionsFrom(['3.0.1'])
   api.use([
     'ecmascript',
     'reactive-dict',
-    'templating@1.3.4',
-    'aldeed:autoform@6.0.0 || 7.0.0'
+    'templating@1.4.4',
+    'aldeed:autoform@8.0.0-rc.3'
   ])
 
   api.mainModule('static.js', 'client')
 })
 
 Package.onTest(function (api) {
+  api.versionsFrom(['3.0.1'])
   api.use('ecmascript')
   api.use('templating')
   api.use('blaze')
